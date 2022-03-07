fca59348fa201c9a843f0685428dc138420ed50f
chocheolhee <whcjfgml92@naver.com>
2022-03-07 12:47:26 +0900

    로그아웃

--- src/main/java/com/study/Hello/HomeController.java
+++ src/main/java/com/study/Hello/HomeController.java
@@ -1,13 +1,33 @@
 package com.study.Hello;
 
+import com.study.Hello.entity.Member;
+import com.study.Hello.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(
+            @CookieValue(name = "memberId", required = false) Long memberId, Model model
+    ) {
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //로그인
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- src/main/java/com/study/Hello/service/LoginService.java
+++ src/main/java/com/study/Hello/service/LoginService.java
@@ -12,7 +12,7 @@ public class LoginService {
     private final MemberRepository memberRepository;
 
     /**
-     * @return null이면 로그인 실패
+     * @return Null 이면 로그인 실패
      */
     public Member login(String loginId, String password) {
         return memberRepository.findByLoginId(loginId)
